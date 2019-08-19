8e48cc14277febac404be98ade8acacf8a698f19
Ian Diaz <idiaz@itba.edu.ar>
2019-08-19 14:07:27 -0300

    Cambie un par de cosas del ex2 y compile todo de nuevo

--- Ex2/test.m
+++ Ex2/test.m
@@ -1,16 +1,17 @@
 function [] = test()
   clear all;
   clc;
-  x = 0:2/100:2;
+  x = 0:2/1000:2;
   y = zeros(1,length(x));
   y = hola(x,1);
-  plot(x,y,'k');
+  %plot(x,y,'k');
   hold on;
-    for  k =2:100
+    for  k =2:1000
     y = y + hola(x,k);
-    plot(x,y,'k');
+    %plot(x,y,'k');
     endfor
   grid on;
+  plot(x,y,'k');
   title("Input Signal de 1 a 100 armonicos");
   xlabel("tiempo/periodo");
   ylabel("Tension");
